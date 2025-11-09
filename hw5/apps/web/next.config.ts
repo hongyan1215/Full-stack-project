a4836fd8c421335e81995cf8609d10fc757db224
Hongyan_Huang <hyhuang891215@gmail.com>
2025-11-09 17:02:38 +0800

    fix: Remove absolute path from next.config.ts for Vercel deployment

--- hw5/apps/web/next.config.ts
+++ hw5/apps/web/next.config.ts
@@ -6,22 +6,12 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "2mb",
     },
   },
-  turbopack: {
-    root: "/Users/hongyan/wp1141/hw5",
-    resolveAlias: {
-      "@ui": "../../packages/ui",
-      "@utils": "../../packages/utils",
-      "@db": "../../packages/db",
-      "@config": "../../packages/config",
-    },
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.pravatar.cc" },
       { protocol: "https", hostname: "res.cloudinary.com" }
     ],
   },
-  allowedDevOrigins: ["192.168.1.111"],
   transpilePackages: ["@ui/shared", "@utils/shared", "@db/client", "@config/shared"],
 };
 
